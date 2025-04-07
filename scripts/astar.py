56758ed6c43d0ef9642e120a1fc159976170706e
itbellix <itbellix@yahoo.com>
2025-04-07 18:58:53 +0200

    make astar code a class

--- scripts/astar.py
+++ scripts/astar.py
@@ -35,118 +35,124 @@ class Node:
         return self.f > other.f
     
 
-def return_path(current_node):
-    path = []
-    current = current_node
-    while current is not None:
-        path.append(current.position)
-        current = current.parent
-    return path[::-1]  # Return reversed path
+class Astar:
+
+    def __init__(self):
+        pass
+
+
+    def return_path(current_node):
+        path = []
+        current = current_node
+        while current is not None:
+            path.append(current.position)
+            current = current.parent
+        return path[::-1]  # Return reversed path
 
     
 
-def astar(maze, start, end, strain, sweight, allow_diagonal_movement=True, allow_3D_map=False):
-    """
-    Returns a list of tuples as a path from the given start to the given end in the given maze
-    :param maze:
-    :param start:
-    :param end:
-    :return:
-    """
-    meanstrain = np.mean(strain)
-    # Create start and end node
-    start_node = Node(None, start)
-    start_node.g = start_node.h = start_node.f = 0
-    end_node = Node(None, end)
-    end_node.g = end_node.h = end_node.f = 0
+    def plan(self, maze, start, end, strain, allow_diagonal_movement=True, allow_3D_map=False):
+        """
+        Returns a list of tuples as a path from the given start to the given end in the given maze
+        :param maze:
+        :param start:
+        :param end:
+        :return:
+        """
+        meanstrain = np.mean(strain)
+        # Create start and end node
+        start_node = Node(None, start)
+        start_node.g = start_node.h = start_node.f = 0
+        end_node = Node(None, end)
+        end_node.g = end_node.h = end_node.f = 0
 
-    # Initialize both open and closed list
-    open_list = []
-    closed_list = []
+        # Initialize both open and closed list
+        open_list = []
+        closed_list = []
 
-    # Heapify the open_list and Add the start node
-    heapq.heapify(open_list)
-    heapq.heappush(open_list, start_node)
+        # Heapify the open_list and Add the start node
+        heapq.heapify(open_list)
+        heapq.heappush(open_list, start_node)
 
-    # Adding a stop condition
-    outer_iterations = 0
-    max_iterations = 2*(len(maze[0]) * len(maze) // 2)
+        # Adding a stop condition
+        outer_iterations = 0
+        max_iterations = 2*(len(maze[0]) * len(maze) // 2)
 
-    # what squares do we search
-    adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0),)
-    if allow_diagonal_movement:
-        adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1),)
+        # what squares do we search
+        adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0),)
+        if allow_diagonal_movement:
+            adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1),)
 
-    if allow_3D_map:
-        max_iterations = (len(maze[0]) * len(maze) * len(maze[2]) // 2)
-        adjacent_squares = ((0, 0, -1), (0, 0, 1), (0, -1, 0), (0, 1, 0), (0, -1, -1), (0, -1, 1), (0, 1, -1), (0, 1, 1),
-                            (-1, -1, 0), (1, 1, 0), (-1, 1, 0), (1, -1, 0), (-1, 0, -1), (1, 0, 1), (-1, 0, 1), (1, 0, -1),
-                            (1, 1, 1), (-1, 1, 1), (1, -1, 1), (1, 1, -1), (-1, -1, 1), (1, -1, -1), (-1, 1, -1), (-1, -1, -1),
-                            (1, 0, 0), (-1, 0, 0))
+        if allow_3D_map:
+            max_iterations = (len(maze[0]) * len(maze) * len(maze[2]) // 2)
+            adjacent_squares = ((0, 0, -1), (0, 0, 1), (0, -1, 0), (0, 1, 0), (0, -1, -1), (0, -1, 1), (0, 1, -1), (0, 1, 1),
+                                (-1, -1, 0), (1, 1, 0), (-1, 1, 0), (1, -1, 0), (-1, 0, -1), (1, 0, 1), (-1, 0, 1), (1, 0, -1),
+                                (1, 1, 1), (-1, 1, 1), (1, -1, 1), (1, 1, -1), (-1, -1, 1), (1, -1, -1), (-1, 1, -1), (-1, -1, -1),
+                                (1, 0, 0), (-1, 0, 0))
 
-    # Loop until you find the end
-    while len(open_list) > 0:
-        outer_iterations += 1
+        # Loop until you find the end
+        while len(open_list) > 0:
+            outer_iterations += 1
 
-        if outer_iterations > max_iterations:
-            # if we hit this point return the path such as it is
-            # it will not contain the destination
-            warn("giving up on pathfinding too many iterations")
-            return return_path(current_node)
+            if outer_iterations > max_iterations:
+                # if we hit this point return the path such as it is
+                # it will not contain the destination
+                warn("giving up on pathfinding too many iterations")
+                return self.return_path(current_node)
 
-            # Get the current node
-        current_node = heapq.heappop(open_list)
-        closed_list.append(current_node)
+                # Get the current node
+            current_node = heapq.heappop(open_list)
+            closed_list.append(current_node)
 
-        # Found the goal
-        if current_node == end_node:
-            return return_path(current_node)
+            # Found the goal
+            if current_node == end_node:
+                return self.return_path(current_node)
 
-        # Generate children
-        children = []
+            # Generate children
+            children = []
 
-        for new_position in adjacent_squares:  # Adjacent squares
+            for new_position in adjacent_squares:  # Adjacent squares
 
-            # Get node position
-            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+                # Get node position
+                node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
 
-            # Make sure within range
-            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (
-                    len(maze[len(maze) - 1]) - 1) or node_position[1] < 0:
-                continue
+                # Make sure within range
+                if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (
+                        len(maze[len(maze) - 1]) - 1) or node_position[1] < 0:
+                    continue
 
-            # Make sure walkable terrain
-            if maze[node_position[0]][node_position[1]] != 0:
-                continue
+                # Make sure walkable terrain
+                if maze[node_position[0]][node_position[1]] != 0:
+                    continue
 
-            # Get Node Strain
-            node_strain = strain[node_position[0]][node_position[1]]  #swapped indices here.. wtf? check other things.
+                # Get Node Strain
+                node_strain = strain[node_position[0]][node_position[1]]  #swapped indices here.. wtf? check other things.
 
-            # Create new node
-            new_node = Node(current_node, node_position, node_strain)
+                # Create new node
+                new_node = Node(current_node, node_position, node_strain)
 
-            # Append
-            children.append(new_node)
+                # Append
+                children.append(new_node)
 
-        # Loop through children
-        for child in children:
-            # Child is on the closed list
-            if len([closed_child for closed_child in closed_list if closed_child == child]) > 0:
-                continue
+            # Loop through children
+            for child in children:
+                # Child is on the closed list
+                if len([closed_child for closed_child in closed_list if closed_child == child]) > 0:
+                    continue
 
-            # Create the f, g, and h values
-            child.g = current_node.g + 20*child.strain #180*meanstrain*child.strain # current_node.g + 1
-            # child.g = current_node.g + child.strain #180*meanstrain*child.strain # current_node.g + 1
-            child.h = (meanstrain* ((child.position[0] - end_node.position[0])) ** 2) + (meanstrain*((child.position[1] - end_node.position[1])) ** 2)
-            child.f = child.g + child.h * (child.strain**2)
+                # Create the f, g, and h values
+                child.g = current_node.g + 20*child.strain #180*meanstrain*child.strain # current_node.g + 1
+                # child.g = current_node.g + child.strain #180*meanstrain*child.strain # current_node.g + 1
+                child.h = (meanstrain* ((child.position[0] - end_node.position[0])) ** 2) + (meanstrain*((child.position[1] - end_node.position[1])) ** 2)
+                child.f = child.g + child.h * (child.strain**2)
 
-            # Child is already in the open list
-            if len([open_node for open_node in open_list if
-                    child.position == open_node.position and child.g > open_node.g]) > 0:
-                continue
+                # Child is already in the open list
+                if len([open_node for open_node in open_list if
+                        child.position == open_node.position and child.g > open_node.g]) > 0:
+                    continue
 
-            # Add the child to the open list
-            heapq.heappush(open_list, child)
-    
-    # if no path is found, return None
-    return None
+                # Add the child to the open list
+                heapq.heappush(open_list, child)
+        
+        # if no path is found, return None
+        return None
