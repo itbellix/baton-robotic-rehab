a7a6e454d5a0a9993cfad9dc52a5eac0d5594cd3
itbellix <itbellix@yahoo.com>
2025-04-15 20:16:48 +0200

    major changes to plotting script (WIP)

--- scripts/plot_graphs_exp2.py
+++ scripts/plot_graphs_exp2.py
@@ -6,6 +6,11 @@ This script produces the results that I aggregated in Fig. 9 of the paper.
 import os
 import pickle
 from spatialmath import SO3
+import numpy as np
+import rosbag
+from scipy.signal import butter, filtfilt
+import matplotlib.pyplot as plt
+from scipy.spatial.transform import Rotation as R
 
 num_params = 6
 def gaussian_2d(x, y, amplitude, x0, y0, sigma_x, sigma_y, offset):
@@ -76,14 +81,14 @@ def generate_approximated_strainmap(file_strainmaps, ar_value, act_value = 0):
 def main():
     # define the required paths
     code_path = os.path.dirname(os.path.realpath(__file__))     # getting path to where this script resides
-    path_to_repo = os.path.join(code_path, '..', '..')          # getting path to the repository
+    path_to_repo = os.path.join(code_path, '..')          # getting path to the repository
     path_to_bag = os.path.join(path_to_repo, 'Personal_Results', 'bags', 'experiment_2')
     # bag_file_name = '2024-05-07-19-05-30_exp2.bag'      # for experiment without activation
-    bag_file_name = '2024-05-08-09-40-46_exp2.bag'    # for experiment with activation
+    bag_file_name = 'exp2_passive_ISI.bag'    # for experiment with activation
 
     # load the strainmap dictionary used in the experiment
     # file_strainmaps = '/home/itbellix/Desktop/GitHub/PTbot_official/Personal_Results/Strains/Passive/AllMuscles/params_strainmaps_num_Gauss_3/params_strainmaps_num_Gauss_3.pkl' 
-    file_strainmaps = '/home/itbellix/Desktop/GitHub/PTbot_official/Personal_Results/Strains/Active/SSPA_params_strainmaps_num_Gauss_3/SSPA_params_strainmaps_active_3gauss.pkl'
+    file_strainmaps = os.path.join(path_to_repo, 'Musculoskeletal Models', 'Strain Maps', 'Active', 'differentiable_strainmaps_ISI.pkl')
 
     # instantiate variables (they will be Mx4 matrices, where M is variable -number of data- and the last column is the timestamp)
     estimated_shoulder_state = None
@@ -104,86 +109,95 @@ def main():
 
         # extracting commanded ee cartesian poses
         print('Extracting commanded ee cartesian poses')
-        for _, msg, time_msg in bag.read_messages(topics=['/optimal_cartesian_ref_ee']):
+        for _, msg, time_msg in bag.read_messages(topics=['/CartesianImpedanceController/reference_cartesian_pose']):
             if xyz_cmd is None:
                 timestamps_cmd = time_msg.to_time()
-                data_msg = np.reshape(msg.data, (4,4))
-                xyz_cmd = np.hstack((data_msg[0:3, 3], timestamps_cmd))
-                angle, vector = SO3(data_msg[0:3, 0:3]).angvec(unit='deg')
+                position_reference = np.array([msg.pose.position.x,msg.pose.position.y, msg.pose.position.z])
+                quat_reference = np.array([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
+                xyz_cmd = np.hstack((position_reference, timestamps_cmd))
+                angle, vector = SO3(R.from_quat(quat_reference, scalar_first=False).as_matrix()).angvec(unit='deg')
                 angvec_cmd = np.hstack((angle, vector, timestamps_cmd))
             else:
                 timestamps_cmd = time_msg.to_time()
-                data_msg = np.reshape(msg.data, (4,4))
-                xyz_cmd = np.vstack((xyz_cmd, np.hstack((data_msg[0:3, 3], timestamps_cmd))))
-                angle, vector = SO3(data_msg[0:3, 0:3]).angvec(unit='deg')
+                position_reference = np.array([msg.pose.position.x,msg.pose.position.y, msg.pose.position.z])
+                quat_reference = np.array([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
+                xyz_cmd = np.vstack((xyz_cmd, np.hstack((position_reference, timestamps_cmd))))
+                angle, vector = SO3(R.from_quat(quat_reference, scalar_first=False).as_matrix()).angvec(unit='deg')
                 angvec_cmd = np.vstack((angvec_cmd, np.hstack((angle, vector, timestamps_cmd))))
 
         print('Extracting not adjusted ee Z cartesian value')
         for _, msg, time_msg in bag.read_messages(topics=['/uncompensated_z_ref']):
             if z_uncompensated is None:
                 timestamps_z = time_msg.to_time()
-                z_uncompensated = np.hstack((msg.data, timestamps_z))
+                z_uncompensated = np.hstack((msg.data[0], timestamps_z))
             else:
                 timestamps_z = time_msg.to_time()
-                z_uncompensated = np.vstack((z_uncompensated, np.hstack((msg.data, timestamps_z))))
+                z_uncompensated = np.vstack((z_uncompensated, np.hstack((msg.data[0], timestamps_z))))
 
         # extracting actual ee cartesian poses
         print('Extracting actual ee cartesian poses')
-        for _, msg, time_msg in bag.read_messages(topics=['/iiwa7/ee_cartesian_pose']):
+        for _, msg, time_msg in bag.read_messages(topics=['/CartesianImpedanceController/cartesian_pose']):
             time_curr = time_msg.to_time()
             if time_curr >= xyz_cmd[0, -1]:
                 if xyz_curr is None:
                     timestamps_curr = time_msg.to_time()
-                    data_msg = np.reshape(msg.pose, (4,4))
-                    xyz_curr = np.hstack((data_msg[0:3, 3], timestamps_curr))
-                    angle, vector = SO3(data_msg[0:3, 0:3]).angvec(unit='deg')
+                    position_curr = np.array([msg.pose.position.x,msg.pose.position.y, msg.pose.position.z])
+                    quat_curr = np.array([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
+                    xyz_curr = np.hstack((position_curr, timestamps_curr))
+                    angle, vector = SO3(R.from_quat(quat_curr, scalar_first=False).as_matrix()).angvec(unit='deg')
                     angvec_curr = np.hstack((angle, vector, timestamps_curr))
                 else:
                     timestamps_curr = time_msg.to_time()
-                    data_msg = np.reshape(msg.pose, (4,4))
-                    xyz_curr = np.vstack((xyz_curr, np.hstack((data_msg[0:3, 3], timestamps_curr))))
-                    angle, vector = SO3(data_msg[0:3, 0:3]).angvec(unit='deg')
+                    position_curr = np.array([msg.pose.position.x,msg.pose.position.y, msg.pose.position.z])
+                    quat_curr = np.array([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
+                    xyz_curr = np.vstack((xyz_curr, np.hstack((position_curr, timestamps_curr))))
+                    angle, vector = SO3(R.from_quat(quat_curr, scalar_first=False).as_matrix()).angvec(unit='deg')
                     angvec_curr = np.vstack((angvec_curr, np.hstack((angle, vector, timestamps_curr))))
 
         print('Extracting optimization outputs')
         optimal_trajectory = None
         optimal_strain = None
-        activation_history = None
         for _, msg, time_msg in bag.read_messages(topics=['/optimization_output']):
             data = np.reshape(msg.data[0:-1], (9, 11))
             time_curr = time_msg.to_time()
             if optimal_strain is None:
                 optimal_strain = np.hstack((data[8,:], time_curr))
                 optimal_trajectory = np.hstack((data[0:6,:], time_curr * np.ones((6,1))))
-                activation_history = np.hstack((msg.data[-1], time_curr))
             else:
                 optimal_strain = np.vstack((optimal_strain, np.hstack((data[8,:], time_curr))))
                 optimal_trajectory = np.vstack((optimal_trajectory, np.hstack((data[0:6,:], time_curr * np.ones((6,1))))))
-                activation_history = np.vstack((activation_history, np.hstack((msg.data[-1], time_curr))))
 
-
-        # extracting joint configurations
-        print('Extracting joint configurations')
-        joint_states = None
-        for _, msg, time_msg in bag.read_messages(topics=['/iiwa7/joint_states']):
+        print('Extracting forces')
+        interaction_force_magnitude = None
+        for _, msg, time_msg in bag.read_messages(topics=['/ft_sensor_data']):
             time_curr = time_msg.to_time()
-            if time_curr >= xyz_cmd[0, -1]:
-                if joint_states is None:
-                    timestamps_curr = time_msg.to_time()
-                    joint_states = np.hstack((np.array(msg.position), timestamps_curr))
-                else:
-                    timestamps_curr = time_msg.to_time()
-                    joint_states = np.vstack((joint_states, np.hstack((np.array(msg.position), timestamps_curr))))
+            if interaction_force_magnitude is None:
+                interaction_force_magnitude = np.hstack((np.linalg.norm(msg.data[0:3]), time_curr))
+            else:
+                interaction_force_magnitude = np.vstack((interaction_force_magnitude, np.hstack((np.linalg.norm(msg.data[0:3]), time_curr))))
 
-        
+        print('Extracting muscle activation')
+        num_muscles = 22
+        index_muscle = 11       # 11 for infraspinatus inferior (ISI), 18 for supraspinatus anterior (SSPA)
+        muscle_activation_max = None
+        muscle_activation_selected = None
+        for _, msg, time_msg in bag.read_messages(topics=['/estimated_muscle_activation']):
+            time_curr = time_msg.to_time()
+            if muscle_activation_max is None:
+                muscle_activation_max = np.hstack((np.max(msg.data[0:num_muscles]), time_curr))
+                muscle_activation_selected = np.hstack((msg.data[index_muscle], time_curr))
+            else:
+                muscle_activation_max = np.vstack((muscle_activation_max, np.hstack((np.max(msg.data[0:num_muscles]), time_curr))))
+                muscle_activation_selected = np.vstack((muscle_activation_selected, np.hstack((msg.data[index_muscle], time_curr))))
+
 
     # reshaping the optimal trajectories into 3D array
     optimal_trajectory = np.reshape(optimal_trajectory, (6, -1, optimal_trajectory.shape[1]), order='F')
 
     # Now, let's filter the data to retain only the interesting part of the experiment
     # (i.e., when the subject is wearing the brace properly and the robot is moving)
-    init_time = xyz_curr[int(xyz_curr.shape[0]/100*45), -1]         # identify initial timestep
-    end_time = xyz_curr[int(xyz_curr.shape[0]/100*92), -1] - init_time # identify final timestep
+    init_time = xyz_curr[int(xyz_curr.shape[0]/100*1), -1]         # identify initial timestep
+    end_time = xyz_curr[int(xyz_curr.shape[0]/100*99), -1] - init_time # identify final timestep
 
     estimated_shoulder_state[:,-1] = estimated_shoulder_state[:,-1] - init_time   # center time values starting at initial time
     xyz_curr[:,-1] = xyz_curr[:,-1] - init_time
@@ -193,8 +207,11 @@ def main():
     angvec_curr[:,-1] = angvec_curr[:,-1] - init_time
     angvec_cmd[:,-1] = angvec_cmd[:,-1] - init_time
     optimal_trajectory[:,:,-1] = optimal_trajectory[:,:,-1] - init_time
-    activation_history[:, -1] = activation_history[:,-1] - init_time
-    joint_states[:, -1] = joint_states[:, -1] - init_time
+    if interaction_force_magnitude is not None:
+        interaction_force_magnitude[:,-1] = interaction_force_magnitude[:,-1] - init_time
+    if muscle_activation_max is not None:
+        muscle_activation_max[:,-1] = muscle_activation_max[:,-1] - init_time
+        muscle_activation_selected[:,-1] = muscle_activation_selected[:,-1] - init_time
 
     estimated_shoulder_state = estimated_shoulder_state[(estimated_shoulder_state[:,-1]>0) & (estimated_shoulder_state[:,-1]<end_time)]
     xyz_curr = xyz_curr[(xyz_curr[:,-1]>0) & (xyz_curr[:,-1]<end_time)]    # retain data after initial time
@@ -203,8 +220,6 @@ def main():
         z_uncompensated = z_uncompensated[(z_uncompensated[:,-1]>0) & (z_uncompensated[:,-1]<end_time)]
     angvec_curr = angvec_curr[(angvec_curr[:,-1]>0) & (angvec_curr[:,-1]<end_time)]
     angvec_cmd = angvec_cmd[(angvec_cmd[:,-1]>0) & (angvec_cmd[:,-1]<end_time)]
-    activation_history = activation_history[(activation_history[:,-1]>0) & (activation_history[:,-1]<end_time)]
-    joint_states = joint_states[(joint_states[:,-1]>0) & (joint_states[:,-1]<end_time)]
 
     filtered_trajectories = []                      # also filter the optimal trajectories
     for i in range(optimal_trajectory.shape[0]):
@@ -236,66 +251,26 @@ def main():
     ax.set_title("EXP2: Trajectory on strainmap (0 activation)")
     fig.colorbar(cb)
 
-    # visualize 2D trajectory on strainmap (varying activation) USING '2024-05-08-09-40-46_exp2.bag'
-    begin_d = int(len_data/100*30.5)
-    end_d = int(len_data/100*63)
-    change_d = int(len_data/100*36.0)
+    # visualize 2D trajectory on strainmap
+    strainmap = generate_approximated_strainmap(file_strainmaps, estimated_shoulder_state[100, 4])
 
-    begin_act = np.abs(activation_history[:,-1]-estimated_shoulder_state[begin_d, -1]).argmin()
-    end_act = np.abs(activation_history[:,-1]-estimated_shoulder_state[end_d, -1]).argmin()-9
-
-    timestamp_change = estimated_shoulder_state[change_d, -1]
-    old_plan = optimal_trajectory[:, np.abs(optimal_trajectory[0,:,-1] - timestamp_change).argmin(), :]
-
-    axial_rot_change = estimated_shoulder_state[np.abs(estimated_shoulder_state[:,1] - timestamp_change).argmin(), 4]
-
-    strainmap_act_change = generate_approximated_strainmap(file_strainmaps, axial_rot_change, activation_history[np.abs(activation_history[:,1] - timestamp_change).argmin(), 0])
-
-    num_data = end_d-begin_d
-    num_repeat = int(np.ceil(num_data / len(activation_history[begin_act:end_act])))
-    activation_to_color = np.repeat(activation_history[begin_act:end_act, 0], num_repeat)[:num_data]
-
+    # visualize 2D trajectory on strainmap
     fig = plt.figure()
     ax = fig.add_subplot()
-    ax.imshow(strainmap_act_change.T, origin='lower', cmap='hot', extent=[-20, 160, 0, 144], vmin=strainmap_act_change.min(), vmax=0.8, zorder = 0)
-    ax.scatter(np.rad2deg(old_plan[0,2:-2]), np.rad2deg(old_plan[2,2:-2]), color = 'white', s = 2, zorder = 2)
-    sc = ax.scatter(
-        np.rad2deg(estimated_shoulder_state[begin_d:end_d, 0]), 
-        np.rad2deg(estimated_shoulder_state[begin_d:end_d, 2]),
-        c=activation_to_color,
-        cmap='viridis',
-        s = 1,
-        zorder = 1
-    )
-    fig.colorbar(sc, ax=ax, label="Activation level")
-    ax.scatter(np.array([45]), np.array([95]), label = 'start', c = 'red', edgecolors='black')
-    ax.scatter(np.array([60]), np.array([60]), label = 'goal', c='green', edgecolors='black')
-    ax.set_ylim(45, 110)
-    ax.set_xlim(30, 80)
-    ax.set_xlabel("Plane of Elevation [°]")
-    ax.set_ylabel("Shoulder Elevation [°]")
+    # cb =ax.imshow(strainmap.T, origin='lower', cmap='hot', extent=[-20, 160, 0, 144], vmin=0, vmax=strainmap.max())
+    # fig.colorbar(cb, ax=ax, label = 'Strain [%]')
+    ax.plot(np.rad2deg(estimated_shoulder_state[:-1, 0]), np.rad2deg(estimated_shoulder_state[:-1, 2]))
+    ax.scatter(np.array([50]), np.array([100]), label = 'start')
+    ax.scatter(np.array([100]), np.array([100]), label = 'goal1')
+    ax.scatter(np.array([50]), np.array([70]), label = 'goal2')
+    ax.scatter(np.array([60]), np.array([110]), label = 'goal3', c='black')
+
+    # plot also the optimal trajectories
+    np.rad2deg(estimated_shoulder_state[begin_d:end_d, 0]), np.rad2deg(estimated_shoulder_state[begin_d:end_d, 2])
+    ax.set_xlabel("Plane of elevation [deg]")
+    ax.set_ylabel("Shoulder elevation [deg]")
     ax.legend()
-    ax.set_title("EXP2: Trajectory on strainmap (increasing activation)")
-
-    # visualize 2D trajectory on strainmap (higher activation) USING '2024-05-08-09-40-46_exp2.bag'
-    strainmap_act0_25 = generate_approximated_strainmap(file_strainmaps, estimated_shoulder_state[100, 4], act_value=0.25)
-
-    begin_d = int(len_data/100*63)
-    end_d = int(len_data/100*98)
-
-    fig = plt.figure()
-    ax = fig.add_subplot()
-    cb = ax.imshow(strainmap_act0_25.T, origin='lower', cmap='hot', extent=[-20, 160, 0, 144], vmin=1.45, vmax=1.8)
-    ax.plot(np.rad2deg(estimated_shoulder_state[begin_d:end_d, 0]), np.rad2deg(estimated_shoulder_state[begin_d:end_d, 2]))
-    ax.scatter(np.array([45]), np.array([95]), label = 'goal', c='green', edgecolors='black')
-    ax.scatter(np.array([60]), np.array([60]), label = 'start', c='red', edgecolors='black')
-    ax.set_ylim(45, 110)
-    ax.set_xlim(30, 80)
-    ax.set_xlabel("Plane of Elevation [°]")
-    ax.set_ylabel("Shoulder Elevation [°]")
-    ax.legend()
-    ax.set_title("EXP2: Trajectory on strainmap (0.25 activation)")
-    fig.colorbar(cb, label="Strain [%]")
+    ax.set_title("EXP1: Trajectory on strainmap (with human)")
 
     # visualize individual human DoF
     fig = plt.figure()
@@ -378,6 +353,33 @@ def main():
     ax.set_xlabel('time [s]')
     ax.legend()
     fig.suptitle("EXP2: EE orientation")
+
+    # visualize magnitude of interaction force
+    if interaction_force_magnitude is not None:
+        fs = 1/(interaction_force_magnitude[1,-1] - interaction_force_magnitude[0, -1])
+        cutoff = 30
+        order = 2
+        nyquist = 0.5 * fs
+        normal_cutoff = cutoff / nyquist
+        b, a = butter(order, normal_cutoff, btype='low', analog=False)
+        interaction_force_magnitude_filt = filtfilt(b, a, interaction_force_magnitude[:,0])
+
+        fig = plt.figure()
+        ax = fig.add_subplot()
+        ax.plot(interaction_force_magnitude[:,-1], interaction_force_magnitude[:,0])
+        ax.plot(interaction_force_magnitude[:,-1], interaction_force_magnitude_filt)
+        ax.set_ylabel('[N]')
+        ax.set_title('Interaction force')
+
+    if muscle_activation_selected is not None:
+        fig = plt.figure()
+        ax = fig.add_subplot()
+        ax.plot(muscle_activation_selected[:,-1], muscle_activation_selected[:,0])
+        ax.set_ylabel('activation')
+        ax.set_title('Supraspinatus Anterior')
+
+        # print to screen the average maximum muscle activation
+        print('Average maximum activation: ', np.mean(muscle_activation_max[:, 0]))
     
     plt.show(block=True)
 
